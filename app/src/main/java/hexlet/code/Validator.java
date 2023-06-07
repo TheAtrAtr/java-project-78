447ad927e97fb6d449643c4afb482b6e856df65b
TheAtrAtr <dyadya_atr@mail.ru>
2023-06-07 13:08:32 +0300

    step5

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,4 +3,5 @@ package hexlet.code;
 public class Validator {
     void string(){}
     void number(){}
+    void map(){}
 }
--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,6 +1,9 @@
 package hexlet.code.schemas;
 
+import java.util.Map;
+
 public class MapSchema extends BaseSchema{
     void required(){}
     void sizeof(int size){}
+    void shape(Map<String, BaseSchema> schemas){}
 }
